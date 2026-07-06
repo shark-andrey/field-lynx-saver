119973a327065c1602c54733164cb9986d2f36ba
shark-andrey <163378908+shark-andrey@users.noreply.github.com>
2026-07-06 15:54:21 +0500

    Update db.py

--- src/db.py
+++ src/db.py
@@ -19,3 +19,7 @@ async def init_db():
     async with AsyncDbSession() as session:
         await session.execute(sa.text(query))
         await session.commit()
+
+
+if __name__ == "__main__":
+    asyncio.run(init_db())
